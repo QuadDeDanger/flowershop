e15f3466a15a2c6b065d9f740fb06821dfd46251
g16m3797 <g16m3797@campus.ru.ac.za.here>
2019-05-31 17:09:34 +0200

    mocks for OrderClass PRICE

--- FlowerShop/Order.cs
+++ FlowerShop/Order.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace FlowerShop
 {
     public class Order : IOrder, IIdentified
     {
-        private List<Flower> flowers;
+        private List<IFlower> flowers;
         private bool isDelivered = false;
         public int Id { get; }
 
         // should apply a 20% mark-up to each flower.
         public double Price {
             get {
-                return 0;
+                return flowers.Sum(x => x.Cost * 1.2);
             }
         }
 
@@ -33,6 +34,7 @@ namespace FlowerShop
 
         public Order(IOrderDAO dao, IClient client)
         {
+            this.flowers = new List<IFlower>();
             Id = dao.AddOrder(client);
             d = dao;
         }
@@ -41,7 +43,7 @@ namespace FlowerShop
         // used when we already have an order with an Id.
         public Order(IOrderDAO dao, IClient client, bool isDelivered = false)
         {
-            this.flowers = new List<Flower>();
+            this.flowers = new List<IFlower>();
             this.isDelivered = isDelivered;
             Client = client;
             Id = dao.AddOrder(client);
@@ -50,7 +52,10 @@ namespace FlowerShop
 
         public void AddFlowers(IFlower flower, int n)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i< n; i++)
+            {
+                flowers.Add(flower);
+            }
         }
 
         public void Deliver()
--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -26,5 +26,27 @@ namespace Tests
             //ASSERT
             od.Received().SetDelivered(Arg.Is(o));
         }
+
+        [Test]
+        public void Test2()
+        {
+            //ARRANGE
+            IOrderDAO od = Substitute.For<IOrderDAO>();
+            IClient c = Substitute.For<IClient>();
+            IOrder o = new Order(od, c);
+            IFlower first = Substitute.For<IFlower>();
+            first.Cost.Returns(5.5);
+            IFlower second = Substitute.For<IFlower>();
+            second.Cost.Returns(8.2);
+            o.AddFlowers(first, 3);
+            o.AddFlowers(second, 6);
+
+            //ACT
+            double v = o.Price;
+
+            //ASSERT
+            Assert.That(v, Is.EqualTo((5.5 * 3 + 8.2 * 6) * 1.2));
+
+        }
     }
 }
